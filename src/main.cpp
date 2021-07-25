f0e31c6e06c122039a26ecefc9ae51dbf6bd7a14
Mike Freislich <mike.freislich@gmail.com>
2021-07-25 20:39:55 +0200

    and working again. :)

--- src/main.cpp
+++ src/main.cpp
@@ -5,52 +5,67 @@
 #define CLOCK_PIN 6
 #define OUTPUT_ENABLE_PIN 3
 
-#define ShiftRegisterPWM_IGNORE_PINS {26,27,29,30}
+#define ShiftRegisterPWM_IGNORE_PINS \
+  {                                  \
+    29, 30,31                        \
+  }
 #include "ShiftRegisterPWM.h"
+#include "SimpleTimer.h"
 
 #pragma region SETUP
-
 void setup()
 {
+  //Serial.begin(57600);
+  
 }
 
 #pragma endregion
-
-class Timer
-{
-private:
-  uint32_t lastTime;
-  uint32_t timeout;  
-public:
-  Timer(uint32_t timeout) { start(timeout); }
-  uint32_t elapsed() { return millis() - lastTime; }
-  void start(uint32_t timeout) { this->lastTime = millis(); this->timeout = timeout; }
-  void cycle() {start(timeout);}
-  bool finished() { return (millis() - lastTime >= timeout); }
-};
 
 void loop()
 {
   ShiftRegisterPWM sr = ShiftRegisterPWM();
   sr.interrupt(ShiftRegisterPWM::UpdateFrequency::Medium);
-  sr.setData(0xFFFFFFFF);   
+  sr.setData(0xFFFFFFFF);
 
   int i = 0;
-  Timer t = Timer(500);  
-  while (true)
-  {    
-    uint8_t val = constrain((((float)sin(millis() / 500.0 + 0 / 8.0 * 2.0 * PI) + 1) * 15),0, 255);
-    sr.setPulseWidth(val);
+  sr.setPulseWidth(20);
+  SimpleTimer t = SimpleTimer(200);
+  SimpleTimer gate = SimpleTimer(50);
+  SimpleTimer longTimer = SimpleTimer(3000);
+  SimpleTimer enterTimer = SimpleTimer(5000);
 
-    if (t.finished()) {         
-      i = (i + 1) % 8;      
-      t.cycle();
-      sr.toggle(i);
-      sr.toggle(8+i);
+  sr.set(31, 2);
+
+  uint8_t brightness = 20;
+  
+  while (true)
+  {
+    sr.flash();
+
+    if (gate.done())
+    {           
       sr.toggle(26);
       sr.toggle(27);
-      sr.toggle(29);      
+      sr.toggle(29);
       sr.toggle(30);
+      brightness = (brightness + 1) % 30;
     }
+
+    if (enterTimer.done()) {
+      sr.set(28, (sr.get(28)+1 % 3));
+    }
+
+    if (longTimer.done()) {
+      sr.set(31, (sr.get(31)+1 % 2 + 1));
+    }
+
+    if (t.done())
+    {      
+      sr.toggle(i);
+      sr.toggle(15 - i);
+      i = (i + 1) % 8;      
+    }
+    
+    sr.setPulseWidth(brightness);
   }
 }
